7946904d16a1c835f52bab2ea06ad9f4e5ab3e9c
Mouliagrawal <165806624+Mouliagrawal@users.noreply.github.com>
2025-07-21 22:56:16 +0530

    Update Assignmentdicttionary.py

--- DataStructure/Assignmentdicttionary.py
+++ DataStructure/Assignmentdicttionary.py
@@ -1,15 +1,9 @@
 # 1. Write a program to add a key and value to a dictionary.
-# Sample Dictionary: {0: 10, 1: 20}
-# Expected Result: {0: 10, 1: 20, 2: 30}
 sample_dict = {0: 10, 1: 20}
 sample_dict[2] = 30
 print("1.", sample_dict)
 
 # 2. Write a program to concatenate the following dictionaries to create a new one.
-# dic1 = {1: 10, 2: 20}
-# dic2 = {3: 30, 4: 40}
-# dic3 = {5: 50, 6: 60}
-# Expected Result: {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
 dic1 = {1: 10, 2: 20}
 dic2 = {3: 30, 4: 40}
 dic3 = {5: 50, 6: 60}
@@ -20,14 +14,13 @@ print("2.", result_dict)
 
 # 3. Write a program to check if a given key already exists in a dictionary.
 check_dict = {1: 10, 2: 20, 3: 30}
-key = 2  # Change key to check
+key = 2  # Change key to c
 if key in check_dict:
     print("3. Key exists")
 else:
     print("3. Key does not exist")
 
-# 4. Write a program to iterate over a dictionary using for loop and print the keys alone,
-# values alone and both keys and values.
+# 4. Write a program to iterate over a dictionary using for loop and print the keys alone, values alone and both keys and values.
 iterate_dict = {1: 10, 2: 20, 3: 30}
 print("4. Keys alone:")
 for k in iterate_dict:
@@ -39,8 +32,7 @@ print("Both keys and values:")
 for k, v in iterate_dict.items():
     print(f"{k}: {v}")
 
-# 5. Write a program to prepare a dictionary where the keys are numbers between 1 and 15 (both included)
-# and the values are square of the keys.
+# 5. Write a program to prepare a dictionary where the keys are numbers between 1 and 15 (both included) and the values are square of the keys.
 squares = {i: i**2 for i in range(1, 16)}
 print("5.", squares)
 
